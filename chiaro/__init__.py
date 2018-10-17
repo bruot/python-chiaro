ce0e5c814adcef9b2e4e234177a8afc640f5ffdc
Nicolas Bruot <coremeltdown@bruot.org>
2018-10-17 09:56:53 +0200

    Adding GPL licence
    This licence also applies to all previous versions of python-chiaro.

--- chiaro/__init__.py
+++ chiaro/__init__.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Tools for Optics11 Chiaro indenter
 
-@author: Nicolas Bruot (https://www.bruot.org/hp/)
-@date: 10 September 2018
-"""
+# This file is part of the python-chiaro software.
+#
+# Copyright 2018 Nicolas Bruot and CNRS (https://www.bruot.org/hp/)
+#
+#
+# python-chiaro is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# python-chiaro is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with python-chiaro.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""Tools for Optics11 Chiaro indenter"""
+
 
 from ._core import *
